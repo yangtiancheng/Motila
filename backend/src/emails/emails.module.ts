a01945a0a475ec03ba965bf28623b79b2254f2d3
Tyson <yangtiancheng0@gmail.com>
2026-03-11 20:15:13 +0800

    [FIX]修复邮件模块RBAC依赖注入导致登录500#task:20260311018

--- backend/src/emails/emails.module.ts
+++ backend/src/emails/emails.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EmailConfigModule } from '../email-config/email-config.module';
+import { ModulesModule } from '../modules/modules.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { RbacModule } from '../rbac/rbac.module';
 import { EmailsController } from './emails.controller';
 import { EmailsService } from './emails.service';
 
 @Module({
-  imports: [PrismaModule, EmailConfigModule],
+  imports: [PrismaModule, EmailConfigModule, RbacModule, ModulesModule],
   controllers: [EmailsController],
   providers: [EmailsService],
 })
